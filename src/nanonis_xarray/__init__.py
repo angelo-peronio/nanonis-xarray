cffbe336e0f3b34c31c00c2d5a7d31ba3635df68
Angelo Peronio <22264609+angelo-peronio@users.noreply.github.com>
2024-07-11 21:53:23 +0200

    __version__

--- src/nanonis_xarray/__init__.py
+++ src/nanonis_xarray/__init__.py
@@ -1,9 +1,12 @@
 """Read a Nanonis .dat file into an xarray Dataset."""
 
+from importlib.metadata import version
+
 from pint import UnitRegistry
 
 unit_registry = UnitRegistry()
 
 from .read_dat import read_dat  # noqa: E402
 
+__version__ = version("nanonis-xarray")
 __all__ = ["read_dat", "unit_registry"]
